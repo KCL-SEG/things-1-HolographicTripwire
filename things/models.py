aa1d7267a29985a2657d9dfe2e7ef5c835bb5d71
Joseph Magen <josephsmagen@gmail.com>
2022-10-21 10:55:20 +0100

    Add Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,12 @@
+from sre_constants import MAX_UNTIL
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField(
+        max_length=48
+    )
+    destription = models.TextField(
+        max_length=516
+    )
+    quantity = models.PositiveIntegerField()
