803b26ce686bb31295a4b34b2bf8ebb0f0536b9c
Joseph Magen <josephsmagen@gmail.com>
2022-10-21 11:53:08 +0100

    Update things model

--- things/models.py
+++ things/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
+    def __init__(self, name="", description="", quantity=1):
+        self.name = name
+        self.destription = description
+        self.quantity = quantity
+    """
     name = models.CharField(
         max_length=48
     )
-    destription = models.TextField(
+    destription = models.CharField(
         max_length=516
     )
     quantity = models.PositiveIntegerField()
+    """
